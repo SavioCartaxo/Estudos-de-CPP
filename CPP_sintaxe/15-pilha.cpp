d55ee6bd9d3dc4e705a14a40da6e5074aa92570e
Sávio Cartaxo <savioferreirabeltrao1@gmail.com>
2025-11-11 15:13:06 -0300

    reupando umas besteiras que eu pensei que estavam upadas

---
+++ CPP_sintaxe/15-pilha.cpp
@@ -0,0 +1,19 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(){
+    stack<int> pilha;
+    // Adiciona
+    pilha.push(20);
+    
+    // Tamanho
+    pilha.size();
+    
+    // Primeiro
+    pilha.top();
+
+    // Removes
+    pilha.pop();
+
+    return 0;
+}
